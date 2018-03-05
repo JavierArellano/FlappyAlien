18074d62e715eec1cc7718d8b422e1c7e994f1fb
Javier Arellano <franjarca@hotmail.com>
2018-03-05 12:38:32 +0100

    Borrado de cosas inutiles

--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -15,23 +15,6 @@ import javax.xml.soap.Text;
 
 public class MainGame extends Game {
 
-    /*
-            <SubTexture height="147" width="53" y="40" x="372" name="snakeLava.png"/>
-
-            <SubTexture height="147" width="52" y="0" x="477" name="snakeLava_ani.png"/>
-
-            <SubTexture height="147" width="53" y="334" x="424" name="snakeLava_dead.png"/>
-
-            <SubTexture height="147" width="53" y="239" x="371" name="snakeLava_hit.png"/>
-
-            <SubTexture height="147" width="53" y="187" x="424" name="snakeSlime.png"/>
-
-            <SubTexture height="147" width="52" y="0" x="425" name="snakeSlime_ani.png"/>
-
-            <SubTexture height="147" width="53" y="92" x="319" name="snakeSlime_dead.png"/>
-
-            <SubTexture height="147" width="53" y="239" x="371" name="snakeSlime_hit.png"/>
-     */
     private AssetManager manager;
     public AssetManager getManager(){
         return manager;
